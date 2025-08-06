aa6533f2731553de4cc891d591b936989433b09a
Joshua Wicaksono <joshua@madindonesia.com>
2025-08-06 17:58:57 +0700

    fix: envloader not set the getenv

--- src/Migrations/EnvLoader.php
+++ src/Migrations/EnvLoader.php
@@ -8,10 +8,14 @@ class EnvLoader
 {
     public function __construct()
     {
-        $root = dirname(__DIR__, 3); // Move up to project root
+        $root = dirname(__FILE__, 4); // Move up to project root
         if (file_exists($root . '/.env')) {
             $dotenv = Dotenv::createImmutable($root);
             $dotenv->safeLoad();  // Does not throw error if .env missing
+
+            foreach ($_ENV as $key => $value) {
+                putenv("{$key}={$value}");
+            }
         }
     }
 }
