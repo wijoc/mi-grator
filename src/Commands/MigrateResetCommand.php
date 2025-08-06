4c2de6af743b9ebc549dee9638b335660af20a69
Joshua Wicaksono <joshua@madindonesia.com>
2025-08-06 16:23:14 +0700

    fix: namespace structure

--- src/Commands/MigrateResetCommand.php
+++ src/Commands/MigrateResetCommand.php
@@ -7,7 +7,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Wijoc\MIGrator\Migration;
+use Wijoc\MIGrator\Migrations\Migration;
 
 class MigrateResetCommand extends Command
 {
